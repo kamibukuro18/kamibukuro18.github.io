2446067d07d92edf0aa2a27081bac5812d5cba8d
ikegai.ryota <kamibukuro18@gmail.com>
2023-12-31 22:52:59 +0900

    修正

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+      import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyBQhAxUH--A93Lf7psNvWq0DEa6kDzYNs4";
-const genAI = new GoogleGenerativeAI(API_KEY);
+      const genAI = new GoogleGenerativeAI("AIzaSyBQhAxUH--A93Lf7psNvWq0DEa6kDzYNs4");
 
-async function generateContent() {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const prompt = "Write a story about a magic backpack."
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
+      async function run() {
+		  // For text-only input, use the gemini-pro model
+		  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-// ボタンクリックイベントを設定
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#generateButton').addEventListener('click', generateContent);
-});
+		  const prompt = document.getElementById('inputData').value;
+
+		  const result = await model.generateContent(prompt);
+		  const response = await result.response;
+		  const text = response.text();
+		  console.log(text);
+		  document.getElementById('response').innerText = text;
+	}
+	window.postData = function(){
+	run();
+	}
