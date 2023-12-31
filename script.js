632e817bd8fcc2a650bfbbb19cd17abef58c24a7
ikegai.ryota <kamibukuro18@gmail.com>
2023-12-31 22:25:49 +0900

    修正

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "...";  // あなたのAPIキーをここに入力
+const API_KEY = "AIzaSyBQhAxUH--A93Lf7psNvWq0DEa6kDzYNs4";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 async function generateContent() {
-    const model = genAI.getGenerativeModel({ model: "MODEL_NAME" });
-    // ここにモデルを使用したコードを追加
-    // 例えば、model.generateContent(...) を使用するなど
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = "Write a story about a magic backpack."
 
-    // レスポンスの処理
-    // 例: console.log(response);
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
 }
 
 // ボタンクリックイベントを設定
