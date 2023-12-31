136de826efc5a568f7478f79043d4a3839c0d243
ikegai.ryota <kamibukuro18@gmail.com>
2024-01-01 08:55:29 +0900

    修正

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
       async function run() {
 		  // For text-only input, use the gemini-pro model
 		  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-
-		  const prompt = document.getElementById('inputData').value+"という名前の人が、2024年に何をするかの年表を、面白おかしく、ポジティブに、月ごとに短くまとめて書いてください。名前の記載は不要です。50文字程度で改行してください。タイトルに「【名前】の2024年」と書いてください。";
+		const nameText = document.getElementById('inputData').value
+		  const prompt = nameText +"という名前の人が、2024年に何をするかの年表を、面白おかしく、ポジティブに、月ごとに短くまとめて書いてください。名前の記載は不要です。50文字程度で改行してください。タイトルに「" + nameText + "の2024年」と書いてください。";
 
 		  const result = await model.generateContent(prompt);
 		  const response = await result.response;
